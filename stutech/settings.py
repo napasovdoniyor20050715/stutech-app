2662b93fd094b78a8e6cd2cf01415b3862fdf257
napasovdoniyor20050715 <napasovdoniyor2005@gmail.com>
2026-05-11 07:07:52 +0500

    Update settings.py

--- stutech/settings.py
+++ stutech/settings.py
@@ -90,3 +90,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CSRF_TRUSTED_ORIGINS = ['https://stutech-app.onrender.com']
