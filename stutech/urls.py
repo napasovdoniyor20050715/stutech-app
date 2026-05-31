86288d1a0cc8fc0dee8099d5ea89a8258ff892a7
napasovdoniyor20050715 <napasovdoniyor2005@gmail.com>
2026-05-31 19:48:36 +0500

    Update urls.py

--- stutech/urls.py
+++ stutech/urls.py
@@ -2,18 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
-]
-
-urlpatterns += i18n_patterns(
     path('boshqaruv-panel/', admin.site.urls),
     path('', include('accounts.urls')),
     path('courses/', include('courses.urls')),
     path('assignments/', include('assignments.urls')),
     path('attendance/', include('attendance.urls')),
     path('grades/', include('grades.urls')),
-    prefix_default_language=False,
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
